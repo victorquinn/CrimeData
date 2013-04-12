342abe3ccd0d86453dcebcb5e8d8947cb772dca3
Victor Quinn <vquinn@ngpvan.com>
2013-04-12 08:18:51 -0400

    Added redis cache for much more performant retrieval

--- scripts/performance.py
+++ scripts/performance.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 
 import timeit
-total_time = timeit.timeit("import urllib; urllib.urlretrieve('http://localhost:5000/crimes/2012', 'crimedata.json')", number=100)
-print total_time
+total_time = timeit.timeit("import urllib; urllib.urlretrieve('http://crimedata.herokuapp.com/crimes/2012', 'crimedata.json')", number=100)
+print total_time/100
