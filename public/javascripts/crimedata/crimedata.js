5aa32bdde23221bf9e1f9c93808183206915c3a1
Victor Quinn <vquinn@ngpvan.com>
2013-04-15 23:15:49 -0400

    Now showing the date for which data is being displayed, also removed now redundant MongoDB lines from the app.js code

--- public/javascripts/crimedata/crimedata.js
+++ public/javascripts/crimedata/crimedata.js
@@ -7,6 +7,7 @@ require.config({
         'backbone': 'lib/backbone-min',
         'underscore': 'lib/underscore-min',
         'd3': 'lib/d3.v3.min',
+        'moment': 'lib/moment.min',
         'spin': 'lib/spin.min',
         'text': 'lib/text',
 
@@ -19,11 +20,12 @@ require([
     'jquery',
     'underscore',
     'd3',
+    'moment',
     'spin',
     'CrimeMap',
     'text!templates/crime-pre.html',
     'backbone'
-], function($, _, d3, Spinner, CrimeMap, crime_pre_template) {
+], function($, _, d3, moment, Spinner, CrimeMap, crime_pre_template) {
 
     var Crime = Backbone.Model.extend({});
 
@@ -35,6 +37,8 @@ require([
 
     // A random day with some crimes. Make interactive later.
     crimesCollection.url = '/api/v1/crimes/2012/04/01';
+
+    $('#crime-date').html(moment('2012/04/01').format('MMM D, YYYY'));
 
     crimesCollection.fetch({
         success: function(crimes) {
