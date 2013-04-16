bf069ba782fa79d4248a5ce242305ae31a19eeed
Victor Quinn <vquinn@ngpvan.com>
2013-04-15 22:57:40 -0400

    First commit with map widget hitting live data

--- public/javascripts/crimedata/app/CrimeMap.js
+++ public/javascripts/crimedata/app/CrimeMap.js
@@ -10,5 +10,20 @@ define([
     // can't have a hard dependency on it. Some more info
     // here: http://vq.io/17CyKLH
 
-    mapbox.auto('map-widget', 'victorquinn.map-1jo85ie8');
+    var CrimeMapView = Backbone.View.extend({
+        id: 'widget-map',
+        render: function() {
+            // Create map
+            var map = mapbox.map('widget-map');
+            map.addLayer(mapbox.layer().id('victorquinn.map-1jo85ie8'));
+
+            // Create and add marker layer
+            var markerLayer = mapbox.markers.layer().features(this.model.toJSON());
+
+            map.addLayer(markerLayer)
+                .setExtent(markerLayer.extent());
+        }
+    });
+
+    return CrimeMapView;
 });
--- public/javascripts/crimedata/crimedata.js
+++ public/javascripts/crimedata/crimedata.js
@@ -25,27 +25,21 @@ require([
     'backbone'
 ], function($, _, d3, Spinner, CrimeMap, crime_pre_template) {
 
-    $('.mainwidget').append("<div id='waiting-spinner'></div>");
-
-    var target = document.getElementById('#waiting-spinner');
-    var spinner = new Spinner({ lines: 10, length: 8, width: 4, radius: 8 }).spin(target);
-
     var Crime = Backbone.Model.extend({});
 
     var CrimesCollection = Backbone.Collection.extend({
         model: Crime
     });
 
-    var crimes = new CrimesCollection();
+    var crimesCollection = new CrimesCollection();
 
     // A random day with some crimes. Make interactive later.
-    crimes.url = '/api/v1/crimes/2012/04/01';
+    crimesCollection.url = '/api/v1/crimes/2012/04/01';
 
-    crimes.fetch({
-        success: function(response) {
-            response.each(function(crime) {
-                $('.mainwidget').append(_.template(crime_pre_template, crime.toJSON()));
-            });
+    crimesCollection.fetch({
+        success: function(crimes) {
+            var crimeMap = new CrimeMap({model: crimes});
+            crimeMap.render();
         }
     });
 });
